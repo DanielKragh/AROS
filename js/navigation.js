4b7c79f0a2ff49b6822e67bdf24b9003e20d9732
Daniel Klitgaard <42070753+DanielKragh@users.noreply.github.com>
2019-04-10 10:03:39 +0200

    navigation burger

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,14 @@
 $(function(){
-    
+    var burger = $("svg.burger");
+    var ul = $("ul");
+
+    burger.on("click", function(){
+        ul.toggleClass("showNavbar");
+    })
+    $(window).on("resize", function(){
+        ul.removeClass("showNavbar");
+    })
+    $(window).on("scroll", function(){
+        ul.removeClass("showNavbar");
+    })
 });
