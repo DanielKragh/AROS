671cf0ea3b16b1ebe797cb13c3712546502d2247
Daniel Klitgaard <42070753+DanielKragh@users.noreply.github.com>
2019-04-12 12:07:32 +0200

    lightBox og mobil galleri

---
+++ js/lightBox.js
@@ -0,0 +1,56 @@
+// Light Box
+$(function(){
+
+    // light box tilpasser sig skærmen
+var scrollDis;
+$('body').scroll(function() {
+  scrollDis = $('body').scrollTop();  
+});
+
+
+// lav elementet
+var $zoomBil = $('<div id="zoomBil"></div>');
+var $divRe = $('<div style="position:relative"></div>')
+var $wrap = $('<div id="wrap"></div>');
+var $image = $('<img class="image" >');
+var $video = $('<video class="video" autoplay="autoplay" loop="loop" muted="muted"></video>');
+var $text = $('<p id="billedText"></p>');
+var $close = $('<figure class="close">&times;</figure>')
+
+// 
+$zoomBil.append($divRe);
+$divRe.append($wrap);
+$wrap.append($image);
+$wrap.append($video);
+$wrap.append($text);
+$wrap.append($close);
+$('body').append($zoomBil);
+
+
+// click event
+$('.img-galleri').on('click', 'a', function(evnet){
+  event.preventDefault();
+  console.log(this)
+  
+
+  if($(this).children().eq(0).is("img")){
+    var imageSrc = $(this).attr('href');
+    $image.attr('src', imageSrc);
+    $video.hide();
+    $image.show();
+  } else{
+    var videoSrc = $(this).attr('href');
+    $video.attr('src', videoSrc);
+    $image.hide();
+    $video.show();
+
+  }
+  
+  $zoomBil.show();
+  
+})
+// click event for close.
+$close.click(function(){
+  $zoomBil.hide();
+})
+});
