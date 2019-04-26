ec70d7632eccb68ceb91d1636af69458e15d856a
Daniel Klitgaard <42070753+DanielKragh@users.noreply.github.com>
2019-04-26 13:07:23 +0200

    skins

--- js/skinSellector.js
+++ js/skinSellector.js
@@ -1,5 +1,16 @@
 $(function(){
     var skins = ["resurser/img/Spil/BoySmall.png", "resurser/img/Spil/VadeDrengen.png"];
-    $("#Player").attr("src", skins[0]);
-    $("#imgSellect").attr("src", skins[1]);
+    var Player = $("#Player");
+    var imgSellect = $(".imgSellect");
+    
+    Player.attr("src", skins[0]);
+    imgSellect.attr("src", skins[1]);
+
+    imgSellect.on("click",function(){
+        var skinSrc = $(this).attr("src");
+        var PlayerSrc = Player.attr("src");
+        Player.attr("src", skinSrc);
+        $(this).attr("src", PlayerSrc);
+        
+    })
 });
